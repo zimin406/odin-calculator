694c1de6a2be1deb19f3ec80ba0816784f7161cb
Gwak Jimin <zimin406@yonsei.ac.kr>
2023-08-14 00:35:15 +0900

    Add HTML elements for keys

---
+++ js/script.js
@@ -0,0 +1,39 @@
+function add (num1, num2) {
+    return num1 + num2;
+}
+
+function subtract (num1, num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1 / num2;
+}
+
+let num1 = null;
+let num2 = null;
+let operator = null;
+
+function operate (num1, num2, operator) {
+    switch (operator) {
+        case "+":
+            add(num1, num2);
+            break;
+        case "-":
+            subtract(num1, num2);
+            break;
+        case "*":
+            multiply(num1, num2);
+            break;
+        case "/":
+            divide(num1, num2);
+            break;
+        default:
+    } 
+}
+
+
